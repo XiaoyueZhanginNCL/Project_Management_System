ba799e569dfa43b77d93dc0d06fa6119c7901f5f
XiaoyueZhanginNCL <zhangxiaoyue2022@gmail.com>
2024-09-02 02:55:49 +0100

    create all model entity

--- src/main/java/com/xiaoyue/projectmanagement/model/User.java
+++ src/main/java/com/xiaoyue/projectmanagement/model/User.java
@@ -1,18 +1,22 @@
 package com.xiaoyue.projectmanagement.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long Id;
+    private Long id;
 
     private String email;
     private String fullName;
